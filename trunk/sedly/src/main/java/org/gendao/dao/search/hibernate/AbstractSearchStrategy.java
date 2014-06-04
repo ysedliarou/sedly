c75e2b9d9bdf961a91c0594425a7e23d72ded022
yauhen.sedliarou@gmail.com <yauhen.sedliarou@gmail.com@1aa21d46-5b4e-3227-b790-aeea86cee602>
2014-06-04 14:19:52 +0000

    generic dao

---
+++ trunk/sedly/src/main/java/org/gendao/dao/search/hibernate/AbstractSearchStrategy.java
@@ -0,0 +1,20 @@
+package org.gendao.dao.search.hibernate;
+
+import org.gendao.dao.search.SearchData;
+import org.gendao.dao.search.SearchStrategy;
+import org.gendao.model.DomainObject;
+import org.hibernate.Session;
+
+import java.io.Serializable;
+
+public abstract class AbstractSearchStrategy<R, SD extends SearchData<T, ID>, T extends DomainObject<ID>, ID extends Serializable>
+        implements SearchStrategy<R, SD, T, ID> {
+
+    @Override
+    public final R search(SD searchData, Object searchProvider) {
+        return hibernateSearch(searchData, (Session) searchProvider);
+    }
+
+    public abstract R hibernateSearch(SD searchData, Session session);
+
+}
